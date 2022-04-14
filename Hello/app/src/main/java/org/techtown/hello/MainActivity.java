99a049c113fce687fc4ecb6d8be155c1f1fa5100
junaem <junh9547@gmail.com>
2022-04-14 23:38:35 +0900

    first commit

---
+++ Hello/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -0,0 +1,32 @@
+package org.techtown.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "확인1 눌림", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+    public void onButton3Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-5165-3067"));
+        startActivity(intent);
+    }
+}
